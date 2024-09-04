37cffae525326c63928040d89ec1b2bb0ceb67e3
Ilia Kalygin <ilia.kalygin@ch.abb.com>
2024-09-04 15:37:18 +0200

    data from the uploaded csv automatically loads into a table- and a line chart view, the views are separeated with 2 tabs

--- main.py
+++ main.py
@@ -1,13 +1,30 @@
 from nicegui import ui
 import pandas as pd
+import plotly.express as px
 from io import StringIO
 
 def handle_upload(event):
     upload_component.visible = False
     content = event.content.read().decode('utf-8')
-    # create table from csv
-    df = pd.read_csv(StringIO(content), delimiter=';', usecols=lambda column: column not in ['Unnamed: 6'])
-    ui.table(columns=[{'name': col, 'label': col, 'field': col} for col in df.columns], rows=df.to_dict(orient='records')).classes('mx-auto')
+    df = pd.read_csv(StringIO(content), delimiter=';')
+    
+    with ui.tabs().classes('w-full') as tabs:
+        table_view = ui.tab('Table View')
+        line_chart = ui.tab('Line Chart View')
+    
+    with ui.tab_panels(tabs, value=line_chart).classes('w-full'):
+        with ui.tab_panel(table_view):
+            # create table from csv
+            df_table = df.drop(columns=['Unnamed: 6'], errors='ignore')
+            ui.table(columns=[{'name': col, 'label': col, 'field': col} for col in df_table.columns], rows=df_table.to_dict(orient='records')).classes('mx-auto')
+        
+        with ui.tab_panel(line_chart):
+            # create plotly graph from csv
+            y_columns = [col for col in df.columns if col not in ['Time', 'Unnamed: 6']]
+            fig = px.line(df, x='Time', y=y_columns,
+                          labels={'value': 'Values', 'variable': 'Variables'},
+                          title='Time Series Data')
+            ui.plotly(fig).classes('mx-auto').style('width: 85vw; height: 80vh;')
 
 with ui.card().classes("mx-auto").style('background: transparent; border: none; box-shadow: none;'):
     upload_component = ui.upload(on_upload=handle_upload).props('accept=".csv"').classes('max-w-full')
