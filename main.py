ef61460afdc35a894f911929369b9ee673a5b239
Ilia Kalygin <ilia.kalygin@ch.abb.com>
2024-09-04 14:52:00 +0200

    after upload the data automatically loads into a table

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from nicegui import ui
+import pandas as pd
+from io import StringIO
+
+def handle_upload(event):
+    upload_component.visible = False
+    content = event.content.read().decode('utf-8')
+    # create table from csv
+    df = pd.read_csv(StringIO(content), delimiter=';', usecols=lambda column: column not in ['Unnamed: 6'])
+    ui.table(columns=[{'name': col, 'label': col, 'field': col} for col in df.columns], rows=df.to_dict(orient='records')).classes('mx-auto')
 
 with ui.card().classes("mx-auto").style('background: transparent; border: none; box-shadow: none;'):
-    ui.upload(on_upload=lambda e: ui.notify(f'Uploaded {e.name}')).classes('max-w-full')
+    upload_component = ui.upload(on_upload=handle_upload).props('accept=".csv"').classes('max-w-full')
 
 ui.run()
