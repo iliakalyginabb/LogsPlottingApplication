7dcc59ce9fc373402c009cf473fb43850ff52b53
EUROPE\CHILKAL <ilia.kalygin@ch.abb.com>
2024-09-13 16:21:08 +0200

    implemented sync functionality between plots, added checkboxes on the signals table tab for that purpose

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ import plotly.express as px
 from io import StringIO
 
 df_global = pd.DataFrame()  # dataframe to store uploaded data
-plot_settings = {}  # stores plot visibility settings for each plot
-df_signals = pd.DataFrame(columns=['csv_filename', 'signal_name', 'plot1', 'plot2', 'plot3', 'plot4', 'sync_x_axis'])  # dataframe to store signal settings
+df_signals = pd.DataFrame(columns=['csv_filename', 'signal_name', 'plot1', 'plot2', 'plot3', 'plot4'])  # dataframe to store signal settings
 
 # function to handle CSV upload
 def handle_upload(event):
@@ -25,7 +24,6 @@ def handle_upload(event):
         'plot2': True,
         'plot3': True,
         'plot4': True,
-        'sync_x_axis': False
     })
 
     # Update the df_signals dataframe
@@ -41,18 +39,18 @@ def handle_upload(event):
 def remove_margins_from_plots(fig):
     # Update layout to remove margins and padding around the plot
     fig.update_layout(
-        margin=dict(l=0, r=0, t=0, b=0),  # No margins
+        margin=dict(l=0, r=0, t=30, b=0)
     )
     return fig
 
 # function to update trace visibility per plot based on the data from df_signals
 def update_visibility():
-    global df_signals, plot_settings
+    global df_signals
 
     # dictionary to map plot ids to plots
     plots = {'plot1': plot1, 'plot2': plot2, 'plot3': plot3, 'plot4': plot4}
 
-    for idx, row in df_signals.iterrows():  # unpack the tuple into index and row
+    for _, row in df_signals.iterrows():  # unpack the tuple into index and row
         signal_name = row['signal_name']
         visible_plots = {
             'plot1': row['plot1'],
@@ -72,9 +70,24 @@ def update_visibility():
 
                 plots[plot_id].update()
 
+# function to handle zoom events and synchronize x-axis
+def on_zoom(event):
+    relayout_data = event.args # axes values
+    if 'xaxis.range[0]' in relayout_data and 'xaxis.range[1]' in relayout_data:
+        x_start = relayout_data['xaxis.range[0]']
+        x_end = relayout_data['xaxis.range[1]']
+        for i, checkbox in enumerate(checkboxes):
+            if checkbox.value != False:
+                figs[i].update_xaxes(range=[x_start, x_end])
+                plots[i].update()
+    elif 'xaxis.autorange' in relayout_data:
+        for i, checkbox in enumerate(checkboxes):
+            if checkbox.value != False:
+                figs[i].update_xaxes(range=None)
+                plots[i].update()
 
 def show_results():
-    global df_global, plot1, plot2, plot3, plot4
+    global df_global, plot1, plot2, plot3, plot4, checkboxes, figs, plots
 
     y_columns = [col for col in df_global.columns if 'Unnamed' not in col and col != 'Time']
 
@@ -84,10 +97,6 @@ def show_results():
 
     # main content
     with ui.row().classes('mx-auto'):
-
-        # toggle to select layout
-        toggle1 = ui.toggle(["1x1", "1x2", "2x1", "2x2"], value="1x1").classes('mx-auto')
-
         # initialize tab panels
         with ui.tabs().classes('w-full') as tabs:
             line_chart = ui.tab('Line Chart View')
@@ -96,54 +105,66 @@ def show_results():
         # create tab panels
         with ui.tab_panels(tabs, value=line_chart).classes('w-full'):
             with ui.tab_panel(line_chart):
+                # toggle to select layout
+                toggle1 = ui.toggle(["1x1", "1x2", "2x1", "2x2"], value="1x1").classes('mx-auto')
+
                 with ui.row().classes('w-full justify-around'):
                     def update_layout():
                         layout = toggle1.value
                         if layout == "1x1":
-                            plot1.style('display: block; width: 85vw; height: 80vh;').classes('mx-auto')
+                            plot1.style('display: block; width: 95vw; height: 80vh;').classes('mx-auto')
                             plot2.style('display: none;')
                             plot3.style('display: none;')
                             plot4.style('display: none;')
                         elif layout == "1x2":
-                            plot1.style('display: block; width: 40vw; height: 80vh;').classes('mx-auto')
-                            plot2.style('display: block; width: 40vw; height: 80vh;').classes('mx-auto')
+                            plot1.style('display: block; width: 45vw; height: 80vh;').classes('mx-auto')
+                            plot2.style('display: block; width: 45vw; height: 80vh;').classes('mx-auto')
                             plot3.style('display: none;')
                             plot4.style('display: none;')
                         elif layout == "2x1":
-                            plot1.style('display: block; width: 85vw; height: 40vh;').classes('mx-auto')
-                            plot2.style('display: block; width: 85vw; height: 40vh;').classes('mx-auto')
+                            plot1.style('display: block; width: 95vw; height: 40vh;').classes('mx-auto')
+                            plot2.style('display: block; width: 95vw; height: 40vh;').classes('mx-auto')
                             plot3.style('display: none;')
                             plot4.style('display: none;')
                         elif layout == "2x2":
-                            plot1.style('display: block; width: 42vw; height: 40vh;').classes('mx-auto')
-                            plot2.style('display: block; width: 42vw; height: 40vh;').classes('mx-auto')
-                            plot3.style('display: block; width: 42vw; height: 40vh;').classes('mx-auto')
-                            plot4.style('display: block; width: 42vw; height: 40vh;').classes('mx-auto')
+                            plot1.style('display: block; width: 45vw; height: 40vh;').classes('mx-auto')
+                            plot2.style('display: block; width: 45vw; height: 40vh;').classes('mx-auto')
+                            plot3.style('display: block; width: 45vw; height: 40vh;').classes('mx-auto')
+                            plot4.style('display: block; width: 45vw; height: 40vh;').classes('mx-auto')
 
                         ui.run_javascript('window.dispatchEvent(new Event("resize"));')
 
                     toggle1.on_value_change(update_layout)
 
                     fig1 = remove_margins_from_plots(px.line(df_global, x='Time', y=y_columns, title='Plot 1'))
-                    plot1 = ui.plotly(fig1).classes('mx-auto').style('display: none;')
+                    plot1 = ui.plotly(fig1).on('plotly_relayout', on_zoom).classes('mx-auto').style('display: none;')
 
                     fig2 = remove_margins_from_plots(px.line(df_global, x='Time', y=y_columns, title='Plot 2'))
-                    plot2 = ui.plotly(fig2).classes('mx-auto').style('display: none;')
+                    plot2 = ui.plotly(fig2).on('plotly_relayout', on_zoom).classes('mx-auto').style('display: none;')
 
                     fig3 = remove_margins_from_plots(px.line(df_global, x='Time', y=y_columns, title='Plot 3'))
-                    plot3 = ui.plotly(fig3).classes('mx-auto').style('display: none;')
+                    plot3 = ui.plotly(fig3).on('plotly_relayout', on_zoom).classes('mx-auto').style('display: none;')
 
                     fig4 = remove_margins_from_plots(px.line(df_global, x='Time', y=y_columns, title='Plot 4'))
-                    plot4 = ui.plotly(fig4).classes('mx-auto').style('display: none;')
+                    plot4 = ui.plotly(fig4).on('plotly_relayout', on_zoom).classes('mx-auto').style('display: none;')
+
+                    figs = [fig1, fig2, fig3, fig4]
+                    plots = [plot1, plot2, plot3, plot4]
 
                     # Update plot visibility based on settings  
                     update_visibility()
 
             # ui.aggrid table with the uploaded data
             with ui.tab_panel(signals_table):
-
                 # data from df_signals
                 data = df_signals.to_dict(orient='records')
+
+                # create checkboxes for sync
+                checkboxes = []
+                with ui.row().classes('mx-auto'):
+                    for i in range(4):
+                        checkbox = ui.checkbox(f'Sync Plot {i+1}', value=False)
+                        checkboxes.append(checkbox)
 
                 # aggrid structure
                 column_defs = [
@@ -153,7 +174,6 @@ def show_results():
                     {"headerName": "Plot 2", "field": "plot2", "cellEditor": "agCheckboxCellEditor", "editable": True},
                     {"headerName": "Plot 3", "field": "plot3", "cellEditor": "agCheckboxCellEditor", "editable": True},
                     {"headerName": "Plot 4", "field": "plot4", "cellEditor": "agCheckboxCellEditor", "editable": True},
-                    {"headerName": "Sync X-axis", "field": "sync_x_axis", "cellEditor": "agCheckboxCellEditor", "editable": True}
                 ]
                 
                 # create aggrid
@@ -166,7 +186,7 @@ def show_results():
 
                 # event handler for grid value change
                 def on_grid_value_change(event):
-                    global df_signals, plot_settings
+                    global df_signals
 
                     # extract data from the event
                     data = event.args['data']
@@ -182,7 +202,6 @@ def show_results():
                         df_signals.loc[mask, 'plot2'] = data['plot2']
                         df_signals.loc[mask, 'plot3'] = data['plot3']
                         df_signals.loc[mask, 'plot4'] = data['plot4']
-                        df_signals.loc[mask, 'sync_x_axis'] = data['sync_x_axis']
 
                     # Update plot visibility based on new settings
                     update_visibility()
